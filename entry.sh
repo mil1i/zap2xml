44df770feee57e14c195a2bddae81a16795f5029
mil1i <mil1i@pm.me>
2022-01-23 03:03:19 -0700

    make this a single run

--- entry.sh
+++ entry.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-# start cron
-#/usr/sbin/crond -f -l 8
-
-while :
-do
-	DATE=`date`
-	/zap2xml.pl -u $USERNAME -p $PASSWORD -U -o /data/$XMLTV_FILENAME $OPT_ARGS
-	echo "Last run time: $DATE"
-	echo "Will run in $SLEEPTIME seconds"
-	sleep $SLEEPTIME
-done
+/zap2xml.pl -u $USERNAME -p $PASSWORD -U -o /data/$XMLTV_FILENAME $OPT_ARGS
